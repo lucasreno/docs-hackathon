d901c13988103ee8bb4f0a187e14316818d1f01a
Lucas Renó <lucasreno9@gmail.com>
2024-07-23 19:46:32 -0300

    fix: ajuste de teste

--- teste-carga/teste/get.js
+++ teste-carga/teste/get.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 import { check, group, sleep } from "k6";
 
-const delta = 2000;
+const delta = 200;
 const BASE_URL = "https://vgglxtghug.execute-api.us-east-1.amazonaws.com";
 const token = 'Bearer eyJraWQiOiI4TGQ5b0QyOGJVbVIrdlY3TXlJZCswOEFIVm82SjJYY0VrM1p1eGF3eWFRPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIwOGIxYTBiNS03N2EyLTQwM2YtYmEyMi1jNTI2YjYxMWJkNDgiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV96RnZSbmJKNFUiLCJjbGllbnRfaWQiOiIyNzhmbWltZnA3cGw0OGhzNTJqbmN0aWhsZyIsIm9yaWdpbl9qdGkiOiI2Njk5NjViMi02YTZlLTRhNzctODM4OC1hODg1MTU5Yzg1MWUiLCJldmVudF9pZCI6Ijg1NTY5ZjgxLTM0ZTctNDM3Yi05MmE0LTUxNjIxYWQ3MTY1MSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE3MjE3NzI0NjEsImV4cCI6MTcyMTc3NjA2MSwiaWF0IjoxNzIxNzcyNDYxLCJqdGkiOiI5NDcwN2VlZC1kNDBmLTQ1ZDEtOWQ1MC0yNTAzZWZjNjA3ZDIiLCJ1c2VybmFtZSI6Im1lZGljbzEifQ.YUQj0mNuYI1WwoJbGFvlEdXhGNctAkVfdDZvY0YU9ybrkTMBdFZjiV4oRNMbVM_52dQhcLNzOCoAy7u-6VlISHgmlNKRMyCGDexRiN31vh1z1npI6aawOgt4W4g87XMqZvq-Sy0RthPECvRgucBQ4gGRJsBUDsEynUlJvm0drI5aQ0QRuXnTcJMYSM---Gu6EQb8x2CgeLeSHmLaswhAGqcd0FECT-NjnXXsAj_kXAH6A_QMsxX60eRjL7SngfTc65YCVqAxucLSTCw6c0N_NOAhfyKQUy3mH-nV5bc-3zbV_kEM_cWIZuMSzniXmDbEQ0WCMOi7hAhstdtM8zuWJQ';
 
@@ -11,12 +11,7 @@ export let options = {
         { duration: '10s', target: delta * 2 },
         { duration: '10s', target: delta * 3 },
         { duration: '10s', target: delta * 4 },
-        { duration: '10s', target: delta * 5 },
-        { duration: '10s', target: delta * 6 },
-        { duration: '10s', target: delta * 7 },
-        { duration: '10s', target: delta * 8 },
-        { duration: '10s', target: delta * 9 },
-        { duration: '10s', target: delta * 10 },
+        { duration: '10s', target: delta * 5 }
     ],
 };
 
--- teste-carga/teste/index.js
+++ teste-carga/teste/index.js
@@ -1,15 +1,16 @@
 import http from "k6/http";
 import { check, group, sleep } from "k6";
 
-const BASE_URL = "http://localhost";
-const PORT_AUTH = ":3332";
-const PORT_CONSULTA = ":3334";
+const BASE_URL = "https://vgglxtghug.execute-api.us-east-1.amazonaws.com";
+const token = 'Bearer eyJraWQiOiI4TGQ5b0QyOGJVbVIrdlY3TXlJZCswOEFIVm82SjJYY0VrM1p1eGF3eWFRPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIwOGIxYTBiNS03N2EyLTQwM2YtYmEyMi1jNTI2YjYxMWJkNDgiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV96RnZSbmJKNFUiLCJjbGllbnRfaWQiOiIyNzhmbWltZnA3cGw0OGhzNTJqbmN0aWhsZyIsIm9yaWdpbl9qdGkiOiI2Njk5NjViMi02YTZlLTRhNzctODM4OC1hODg1MTU5Yzg1MWUiLCJldmVudF9pZCI6Ijg1NTY5ZjgxLTM0ZTctNDM3Yi05MmE0LTUxNjIxYWQ3MTY1MSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE3MjE3NzI0NjEsImV4cCI6MTcyMTc3NjA2MSwiaWF0IjoxNzIxNzcyNDYxLCJqdGkiOiI5NDcwN2VlZC1kNDBmLTQ1ZDEtOWQ1MC0yNTAzZWZjNjA3ZDIiLCJ1c2VybmFtZSI6Im1lZGljbzEifQ.YUQj0mNuYI1WwoJbGFvlEdXhGNctAkVfdDZvY0YU9ybrkTMBdFZjiV4oRNMbVM_52dQhcLNzOCoAy7u-6VlISHgmlNKRMyCGDexRiN31vh1z1npI6aawOgt4W4g87XMqZvq-Sy0RthPECvRgucBQ4gGRJsBUDsEynUlJvm0drI5aQ0QRuXnTcJMYSM---Gu6EQb8x2CgeLeSHmLaswhAGqcd0FECT-NjnXXsAj_kXAH6A_QMsxX60eRjL7SngfTc65YCVqAxucLSTCw6c0N_NOAhfyKQUy3mH-nV5bc-3zbV_kEM_cWIZuMSzniXmDbEQ0WCMOi7hAhstdtM8zuWJQ';
+const PORT_AUTH = "";
+const PORT_CONSULTA = "";
 const SENHA_DEFAULT = "123456";
 
 export let options = {
     stages: [
+        { duration: '10s', target: 2 },
         { duration: '10s', target: 5 },
-        { duration: '30s', target: 20 },
         { duration: '10s', target: 0 },
     ],
 };
@@ -30,43 +31,43 @@ export default () => {
             http.post(`${BASE_URL}${PORT_AUTH}/autenticacao/cadastrar/medico`, body);
         }
         check(res => res.status === 200, { "status is 200": res => res.status === 200 });
-        sleep(1);
+        sleep(3);
     });
 
     group("autenticar", () => {
         if (headOrTail()) {
             const cpf = generateRandomCPF();
             const body = { cpf, senha: SENHA_DEFAULT };
-            http.post(`${BASE_URL}${PORT_AUTH}/autenticacao/autenticar/paciente`, body);
+            http.post(`${BASE_URL}${PORT_AUTH}/autenticacao/autenticar/paciente`);
         } else {
             const crm = generateRandomCRM();
             const body = { crm, senha: SENHA_DEFAULT };
-            http.post(`${BASE_URL}${PORT_AUTH}/autenticacao/autenticar/medico`, body);
+            http.post(`${BASE_URL}${PORT_AUTH}/autenticacao/autenticar/medico`);
         }
-        check(res => res.status === 200, { "status is 200": res => res.status === 200 });
-        sleep(1);
+        check(res => res.status === 400, { "status is 400": res => res.status === 400 });
+        sleep(3);
     });
 
     group("cadastrar_agenda", () => {
         const crm = generateRandomCRM();
         const horario = generateRandomHorario();
         const body = { crm, horario };
-        http.post(`${BASE_URL}${PORT_CONSULTA}/agenda/cadastrar`, body);
+        http.post(`${BASE_URL}${PORT_CONSULTA}/agenda/cadastrar`, body, { headers: { Authorization: token } });
         check(res => res.status === 201, { "status is 201": res => res.status === 201 });
-        sleep(1);
+        sleep(3);
     });
 
     group("buscar_agenda", () => {
-        http.get(`${BASE_URL}${PORT_CONSULTA}/agenda/buscar`);
+        http.get(`${BASE_URL}${PORT_CONSULTA}/agenda/buscar`, { headers: { Authorization: token } });
         check(res => res.status === 200, { "status is 200": res => res.status === 200 });
-        sleep(1);
+        sleep(3);
     });
 
     group("listar_consultas", () => {
         let busca = headOrTail() ? generateRandomCRM() : generateRandomCPF();
-        http.get(`${BASE_URL}${PORT_CONSULTA}/consulta/listar?crmOrCpf=${busca}`);
+        http.get(`${BASE_URL}${PORT_CONSULTA}/consulta/listar?crmOrCpf=${busca}`, { headers: { Authorization: token } });
         check(res => res.status === 200, { "status is 200": res => res.status === 200 });
-        sleep(1);
+        sleep(3);
     });
 
     function generateRandomCPF() {
